037d8c6caf78777d1df4e1c6df19e9f71352a5d6
Gautam Raj <gautam.raj@landmarkgroup.in>
2021-10-11 14:29:13 +0530

    Summation of numbers in a list

---
+++ Learn/main/Assignments/assignment_001/SummationOfNumbers.go
@@ -0,0 +1,24 @@
+package assignment001
+
+import (
+	"container/list"
+	"fmt"
+	"strconv"
+)
+
+func main() {
+
+	listOfNumbers := list.New()
+	listOfNumbers.PushFront(5)
+	listOfNumbers.PushBack(4)
+	var sum = 0
+	for i := listOfNumbers.Front(); i != nil; i = i.Next() {
+		fmt.Print(i.Value, ",")
+		value := fmt.Sprintf("%v", i.Value)
+		intType, _ := strconv.Atoi(value)
+		sum = sum + intType
+
+	}
+
+	fmt.Println("\n\n\nSummation of all numbers : ", sum)
+}
